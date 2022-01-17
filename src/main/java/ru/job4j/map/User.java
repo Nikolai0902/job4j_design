d3f2111cbb2943fc5c6bebc5f1310d57cf4d94d0
Nikolai0902 <k1o2l3i49494>
2022-01-17 21:26:50 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,11 +1,14 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy MMM dd");
 
     public User() {
     }
@@ -14,5 +17,28 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + sdf.format(birthday.getTime()) + '}';
+    }
+
+    public static void main(String[] args) {
+        Calendar calendar = new GregorianCalendar(2021, 03, 17);
+        User nik = new User("Nikolai", 2,   calendar);
+        User nikTwo = new User("Nikolai", 2,  calendar);
+
+        System.out.println(nik.hashCode());
+        System.out.println(nikTwo.hashCode());
+
+        Map<User, Object> map = new HashMap<>();
+
+        map.put(nik, new Object());
+        map.put(nikTwo, new Object());
+
+        System.out.println(map);
     }
 }
