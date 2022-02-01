ae11acd5c754d1bad5f3073fbc52a16d23cc9e96
Nikolai0902 <k1o2l3i49494>
2022-02-02 00:31:30 +0300

    0.1. FileOutputStream. [#252488 #259047]

--- src/main/java/ru/job4j/io/Matrix.java
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -14,17 +14,13 @@ public class Matrix {
         return table;
     }
 
-    public static String rsl(int[][] table) {
-        String rsl = "";
-        for (int i = 0; i < table.length; i++) {
-            rsl += Arrays.toString(table[i]);
-        }
-        return rsl;
-    }
-
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("matrix.txt")) {
-            out.write(rsl(Matrix.matrix(5)).getBytes());
+            int[][] table = Matrix.matrix(5);
+            for (int i = 0; i < table.length; i++) {
+                out.write(Arrays.toString(table[i]).getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
             out.write(System.lineSeparator().getBytes());
         } catch (Exception e) {
             e.printStackTrace();
