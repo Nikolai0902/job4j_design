79710dc0bc602fb7f2d0711ddf6e76196e7ece4f
Nikolai0902 <k1o2l3i49494>
2022-02-06 13:01:54 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,51 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+            toString().lines().filter(s -> s.contains("="))
+                    .map(n -> n.trim().split("=", 2))
+                    .forEach(r -> {
+                        if (r[0].isEmpty() || r[1].isEmpty()) {
+                            throw new IllegalArgumentException("Ошибка данных");
+                        }
+                        values.put(r[0], r[1]);
+                    });
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        Config config = new Config("app.properties");
+        config.load();
+        System.out.println(config.value("hibernate.dialect"));
+    }
+
+}
