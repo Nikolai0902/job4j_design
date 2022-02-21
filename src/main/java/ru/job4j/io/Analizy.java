e8e34cc87a496c48cfe624c2948c474b41bc4183
Nikolai0902 <k1o2l3i49494>
2022-02-21 23:42:21 +0300

    2. Анализ доступности сервера. [#859 #265759]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -17,19 +17,12 @@ public class Analizy {
                     isWork = true;
                 }
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-/*        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }*/
         Analizy result = new Analizy();
         result.unavailable("server.log", "target.csv");
     }
