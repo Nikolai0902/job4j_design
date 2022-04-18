6c7706dbdf4ebb1a541e648522f895f34b5f316a
Nikolai <kbus94@yandex.ru>
2022-04-18 17:20:24 +0300

    4.0. File [#252491 #290419]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -13,7 +13,7 @@ public class Dir {
         }
         System.out.println(String.format("size : %s", file.getTotalSpace()));
         for (File subfile : file.listFiles()) {
-            System.out.println(subfile.getName() + " - " + subfile.length());
+            System.out.println(String.format("%s - %s", subfile.getName(), subfile.length()));
         }
     }
 }
