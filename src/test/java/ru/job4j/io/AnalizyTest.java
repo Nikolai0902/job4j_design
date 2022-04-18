e4f3a5f3ffbab6b841fc60b60c03aeca292804d9
Nikolai <kbus94@yandex.ru>
2022-04-18 13:06:25 +0300

    3.0. Тестирование IO [#173905 #268359]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -14,7 +14,7 @@ public class AnalizyTest {
     public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void drop() throws IOException {
+    public void unavailable() throws IOException {
         File sourceIn = folder.newFile("source.txt");
         File target = folder.newFile("target.txt");
         try (PrintWriter out = new PrintWriter(sourceIn)) {
@@ -35,4 +35,20 @@ public class AnalizyTest {
                 + "11:01:02;11:02:08"));
     }
 
+    @Test
+    public void unavailableEmpty() throws IOException {
+        File sourceIn = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(sourceIn)) {
+            out.println("200 10:56:01\n"
+                    + "300 11:02:08");
+        }
+        Analizy result = new Analizy();
+        result.unavailable(sourceIn.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is(""));
+    }
 }
