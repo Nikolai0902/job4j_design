3d0bd88ddf0b76444da053c1e17e75605e54b2f2
Nikolai <kbus94@yandex.ru>
2022-06-01 12:34:45 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/json/Owner.java
+++ src/main/java/ru/job4j/serialization/json/Owner.java
@@ -1,37 +1,24 @@
 package ru.job4j.serialization.json;
 
+import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "owen")
 public class Owner {
+
+    @XmlAttribute
     private String name;
+    @XmlAttribute
     private String surname;
+    @XmlAttribute
     private String dateBr;
+
+    public Owner() {
+    }
 
     public Owner(String name, String surname, String dateBr) {
         this.name = name;
         this.surname = surname;
-        this.dateBr = dateBr;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getDateBr() {
-        return dateBr;
-    }
-
-    public void setDateBr(String dateBr) {
         this.dateBr = dateBr;
     }
 
---
+++ src/main/java/ru/job4j/serialization/xml/Home.java
@@ -0,0 +1,78 @@
+package ru.job4j.serialization.xml;
+
+
+import ru.job4j.serialization.json.Owner;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Arrays;
+
+@XmlRootElement(name = "home")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Home {
+
+    @XmlAttribute
+    private boolean target;
+
+    @XmlAttribute
+    private int rooms;
+    private String street;
+    private Owner owner;
+
+    @XmlElementWrapper(name = "systemSafes")
+    @XmlElement(name = "systemSafe")
+    private String[] systemSafe;
+
+    public Home() {
+    }
+
+    public Home(boolean target, int rooms, String street, Owner owner, String[] systemSafe) {
+        this.target = target;
+        this.rooms = rooms;
+        this.street = street;
+        this.owner = owner;
+        this.systemSafe = systemSafe;
+    }
+
+    @Override
+    public String toString() {
+        return "Home{" + "target=" + target + ", rooms="
+                + rooms + ", street='" + street + '\'' + ", owner="
+                + owner + ", systemSafe=" + Arrays.toString(systemSafe) + '}';
+    }
+
+    /**
+     * Получаем контекст для доступа к АПИ context
+     * Указываем, что нам нужно форматирование marshaller
+     * Сериализуем
+     * Для десериализации нам нужно создать десериализатор unmarshaller
+     * десериализуем
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        final Home home = new Home(true, 4,
+                "Red", new Owner("Nik", "Ivanov", "02.02.1995"),
+                new String[] {"Fire", "Alarm "});
+
+        JAXBContext context = JAXBContext.newInstance(Home.class);
+
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(home, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Home result = (Home) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
+    }
+}
