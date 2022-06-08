0dad3d8026e14408ae820b8cb8a70ab0c40def90
Nikolai <kbus94@yandex.ru>
2022-06-08 15:26:55 +0300

    2. Группировка и агрегатные функции [#373618 #310348]

--- src/main/java/ru/job4j/SQL/devices_groop.sql
+++ src/main/java/ru/job4j/SQL/devices_groop.sql
@@ -1,3 +1,32 @@
+create table devices(
+    id serial primary key,
+    name varchar(255),
+    price float
+);
+
+create table people(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table devices_people(
+    id serial primary key,
+    device_id int references devices(id),
+    people_id int references people(id)
+);
+
+insert into devices(name, price) values ('iphone', 10000);
+insert into devices(name, price) values ('nokia', 80000);
+insert into devices(name, price) values ('fly', 40000);
+
+insert into people(name) values ('Mike');
+insert into people(name) values ('Nik');
+insert into people(name) values ('Ben');
+
+insert into devices_people(device_id, people_id) values (2, 3);
+insert into devices_people(device_id, people_id) values (1, 2);
+insert into devices_people(device_id, people_id) values (3, 1);
+
 --Используя агрегатные функции вывести среднюю цену устройств.
 select avg(price) from devices;
 
