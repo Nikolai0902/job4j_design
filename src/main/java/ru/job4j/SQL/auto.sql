a4c364e12ffcd438c2c24aef1a10a1bcd4573c34
Nikolai <kbus94@yandex.ru>
2022-06-09 09:20:10 +0300

    Хранилище машин [#1733 #310513]

--- src/main/java/ru/job4j/SQL/auto.sql
+++ src/main/java/ru/job4j/SQL/auto.sql
@@ -47,9 +47,9 @@ INSERT INTO car(name, body_id, engine_id, transmission_id) VALUES('Сузуки'
 --Вывести список всех машин и все привязанные к ним детали.
 SELECT c.name "марка",b.name "кузов", e.name "двигатель", t.name "КПП" 
 FROM car c
-JOIN body b ON c.body_id = b.id
-JOIN engine e ON c.engine_id = e.id
-JOIN transmission t ON c.transmission_id = t.id;
+LEFT JOIN body b ON c.body_id = b.id
+LEFT JOIN engine e ON c.engine_id = e.id
+LEFT JOIN transmission t ON c.transmission_id = t.id;
 
 --Вывести отдельно детали (1 деталь - 1 запрос), которые не используются в машине.
 SELECT b.name "кузов" FROM body b
@@ -57,11 +57,11 @@ LEFT JOIN car c ON b.id = c.body_id
 WHERE c.body_id IS NULL;
 
 SELECT e.name "двигатель" FROM engine e
-LEFT JOIN car c ON e.id = c.body_id
+LEFT JOIN car c ON e.id = c.engine_id
 WHERE c.body_id IS NULL;
 
 SELECT t.name "КПП" FROM transmission t
-LEFT JOIN car c ON t.id = c.body_id
+LEFT JOIN car c ON t.id = c.transmission_id
 WHERE c.body_id IS NULL;
 
 
