b47daa7be0d7e8593782582ea8b367e703066202
Nikolai <kbus94@yandex.ru>
2022-06-15 13:02:40 +0300

    0.2. PrepareStatement [#379307 #310895]

--- src/main/java/spammer/ImportDB.java
+++ src/main/java/spammer/ImportDB.java
@@ -26,8 +26,14 @@ public class ImportDB {
                     .lines()
                     .map(s -> s.split(";"))
                     .filter(k -> {
-                        if ("".equals(k[0]) || k.length <= 1) {
+                        if (k.length != 2) {
                             throw new IllegalArgumentException("Ошибка данных");
+                        }
+                        return true;
+                    })
+                    .filter(t -> {
+                        if (t[0].isBlank() || t[1].isBlank()) {
+                            throw new IllegalArgumentException("Одно из значений отсутсвует");
                         }
                         return true;
                     })
